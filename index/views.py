794c2d956a6a9f2c245d0538096876c4308753ad
AleksandrZhukovin <sashokzuk@gmail.com>
2025-11-27 19:32:31 +0200

    add(static): Set up static files

--- index/views.py
+++ index/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', {"article": {"date": "12.11.2024", "title": "Text here", "author": "<NAME>"}})
 
 
 def about(request):
