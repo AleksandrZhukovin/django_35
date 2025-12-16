d55578c40517bf9fa0c7258d8855efdb9b9b6ff5
AleksandrZhukovin <sashokzuk@gmail.com>
2025-12-16 19:29:34 +0200

    add(user): Add user sign up

--- account/views.py
+++ account/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from account.forms import RegistrationForm
+from account.models import User
 
 
 def register(request):
     form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            User.objects.create(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password1']
+            )
+            return redirect('login/')
     return render(request, 'account/register.html', {'form': form})
