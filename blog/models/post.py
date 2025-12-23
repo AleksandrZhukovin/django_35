842793ab05e68386dca91bd3264bd44be89fd1ce
AleksandrZhukovin <sashokzuk@gmail.com>
2025-12-23 19:26:43 +0200

    add(posts): Add image field

--- blog/models/post.py
+++ blog/models/post.py
@@ -4,5 +4,8 @@ from core.models import TimeStampedModelMixin
 
 
 class Post(TimeStampedModelMixin):
-    user = models.ForeignKey("account.User", on_delete=models.CASCADE)
+    user = models.ForeignKey("account.User", on_delete=models.CASCADE, related_name="posts")
+    mentioned_users = models.ManyToManyField("account.User", related_name="mentioned_posts")
+
     content = models.TextField()
+    image = models.ImageField(upload_to="posts/")
