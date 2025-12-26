3ed89c9a6f914a5c2d648d2322a337d6bd0fd0ee
AleksandrZhukovin <sashokzuk@gmail.com>
2025-12-26 19:29:44 +0200

    add(user): Add following logic

--- account/models.py
+++ account/models.py
@@ -2,7 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
 class User(AbstractUser):
-    pass
+    follows = models.ManyToManyField("self", related_name='followers')
+    followers = models.ManyToManyField("self", related_name='following')
+
+    def follow(self, user):
+        user.followers.add(self)
+        self.follows.add(user)
+
+    def unfollow(self, user):
+        user.followers.remove(self)
+        self.follows.remove(user)
--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,6 @@ def create_post(request):
 def home(request):
     posts = Post.objects.all()  # QuerySet [1, 2, 3, 4, ....]
     #  Post.objects.filter(user=request.user, content="Hello")
-    #  Post.objects.get(content="Hello")  object, instance   <--- Get must return 1 instance, returned multiple instead
+    #  Post.objects.get(content="Hello")  object, instance   <- Get must return 1 instance, returned multiple instead
     #  Post.objects.get(id=2)
     return render(request, "blog/home.html", {"posts": posts})
