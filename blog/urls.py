67ce57fc88cce00f855e78ccdec4428eacf2ee5c
AleksandrZhukovin <sashokzuk@gmail.com>
2026-01-27 19:22:49 +0200

    refactor(views): Start using class-based views in blog app

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home),
-    path('posts/create/', views.create_post),
+    path('home/', views.PostsListView.as_view()),
+    path('posts/create/', views.PostCreateView.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 
 from .forms import PostForm
 from .models import Post
@@ -13,9 +15,22 @@ def create_post(request):
     return render(request, 'blog/posts/create_post.html', {"form": form})
 
 
-def home(request):
-    posts = Post.objects.all().prefetch_related('comments').select_related('user')  # QuerySet [1, 2, 3, 4, ....]
-    #  Post.objects.filter(user=request.user, content="Hello")
-    #  Post.objects.get(content="Hello")  object, instance   <- Get must return 1 instance, returned multiple instead
-    #  Post.objects.get(id=2)
-    return render(request, "blog/home.html", {"posts": posts})
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/posts/create_post.html'
+    form_class = PostForm
+    success_url = '/home'
+
+
+# def home(request):
+#     posts = Post.objects.all().prefetch_related('comments').select_related('user')  # QuerySet [1, 2, 3, 4, ....]
+#     #  Post.objects.filter(user=request.user, content="Hello")
+#     #  Post.objects.get(content="Hello")  object, instance   <- Get must return 1 instance, returned multiple instead
+#     #  Post.objects.get(id=2)
+#     return render(request, "blog/home.html", {"posts": posts})
+
+
+class PostsListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
